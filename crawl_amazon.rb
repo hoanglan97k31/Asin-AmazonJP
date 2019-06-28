545bb17fc86b8abbaf9bdbaf4c72937db496f72b
Le Hoang Lan <lan.le@tomosia.com>
2019-06-28 16:10:19 +0700

    Add exception

--- crawl_amazon.rb
+++ crawl_amazon.rb
@@ -1,17 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
-# require 'pry'
 require 'csv'
 
 class Notice
 
-  ## NOTICE
   def insert_keyword
     "= = = = = = = = = = INSERT KEYWORD = = = = = = = = = = = = = = = ="
   end
 
   def insert_last_page
-    "= = = = = = = = = = INSERT LAST PAGE = = = = = = = = = = = = = = = ="
+    "= = = = = = = = = = INSERT LAST PAGE = = = = = = = = = = = = = = ="
   end
 
   def nil_keyword
@@ -40,14 +38,14 @@ class Notice
 end
 
 class InsertData < Notice
-    attr_accessor :keyword, :last_page
+  attr_accessor :keyword, :last_page
 
-    def initialize
-      puts insert_keyword
-      @keyword = gets.chomp.to_s
-      puts insert_last_page
-      @last_page = gets.chomp.to_i
-    end
+  def initialize
+    puts insert_keyword
+    @keyword = gets.chomp.to_s
+    puts insert_last_page
+    @last_page = gets.chomp.to_i
+  end
 end
 
 
@@ -55,47 +53,60 @@ class GetdataAsin
   attr_accessor :urls
   
   def initialize(insert_data)
+    $retries = 4
     @urls = []
     $total_asin = []
     insert_data.last_page.times {|i| urls.push("https://www.amazon.co.jp/s?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&i=aps&k=#{insert_data.keyword}&page=#{i+1}&ref=nb_sb_noss&url=search-alias%3Daps")}
   end
 end
 
-class Parser < Notice
-
-  def initialize(insert_data)
-    ## Function
-    insert_data.urls.each { |url|
-      raw_page = Nokogiri::HTML(open(url))
-      items = raw_page.css("#a-page > #search > .sg-row > .sg-col > .sg-col-inner > .rush-component > .s-result-list .s-result-item")
-      asin_array = []
-      items.each do |item|
-        asin = item.attr("data-asin")
-        unless asin == nil
-          asin_array << asin
-        end
-      end
-      $total_asin = $total_asin + asin_array
-      puts loading
-    }
-    puts complete_get_asin
-    puts insert_to_csv
-    binding.pry
-  end
-end
-
-class InsertCsv < Notice
-  def initialize(insert_data)
-    CSV.open("#{insert_data.keyword}_asin_amazonjp.csv", 'wb') do |csv|
-      $total_asin.each do |item|
-        csv << [item, "\n"]
-      end
-    end
-    puts complete
-  end
-end
-
 insert_data = InsertData.new
 get_data = GetdataAsin.new(insert_data)
+
+begin
+
+  class Parser < Notice
+
+    def initialize(insert_data)
+      ## Function
+      insert_data.urls.each { |url|
+        raw_page = Nokogiri::HTML(open(url))
+        items = raw_page.css("#a-page > #search > .sg-row > .sg-col > .sg-col-inner > .rush-component > .s-result-list .s-result-item")
+        asin_array = []
+        items.each do |item|
+          asin = item.attr("data-asin")
+          unless asin == nil
+            asin_array << asin
+          end
+        end
+        $total_asin = $total_asin + asin_array
+        print loading
+      }
+      puts complete_get_asin
+      puts insert_to_csv
+    end
+  end
+
+  class InsertCsv < Notice
+    def initialize(insert_data)
+      CSV.open("#{insert_data.keyword}_asin_amazonjp.csv", 'wb') do |csv|
+        $total_asin.each do |item|
+          csv << [item, "\n"]
+        end
+      end
+      puts complete
+    end
+  end
+
 parser_data = Parser.new(get_data)
 InsertCsv.new(insert_data)
+
+rescue => exception
+  puts exception
+  puts "Wait for 3 minutes and Retry"
+  sleep(180) # 3 minutes
+  $retries -= 1
+  binding.pry
+  retry if $retries > 0 
+  return puts "#{exception}, Try Again Later" 
+end
