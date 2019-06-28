c7f35a9882f2f654e34b4ff021c781a56f186e32
Le Hoang Lan <lan.le@tomosia.com>
2019-06-28 14:25:41 +0700

    Optimize code

--- crawl_amazon.rb
+++ crawl_amazon.rb
@@ -1,38 +1,100 @@
 require 'nokogiri'
-require 'pry'
 require 'open-uri'
+require 'pry'
 require 'csv'
 
-## Default 
-insert_keyword =   "= = = = = = = = = = INSERT KEYWORD = = = = = = = = = = = = = = = ="
-insert_last_page = "= = = = = = = = = = INSERT LAST PAGE = = = = = = = = = = = = = = ="
-nil_keyword =      "= = = = = = = = = = KEYWORD NILL = = = = = = = = = = = = = = = = ="
-not_availability_last_page = "= = = = = = = = = = LAST PAGE NOT AVAILABILITY = = = = = = = = = ="
+class NoticeDefault
 
-## Insert Keyword
-puts insert_keyword
-keyword = gets.chomp.to_s
-return puts nil_keyword if keyword.empty? 
+  ## NOTICE
+  def insert_keyword
+    "= = = = = = = = = = INSERT KEYWORD = = = = = = = = = = = = = = = ="
+  end
 
-## Insert Page
-puts insert_last_page
-last_page = gets.chomp.to_i
-return puts not_availability_last_page if last_page.abs.nil? || last_page.zero?
+  def insert_last_page
+     "= = = = = = = = = = INSERT LAST PAGE = = = = = = = = = = = = = = = ="
+  end
 
+  def nil_keyword
+    "= = = = = = = = = = KEYWORD NILL = = = = = = = = = = = = = = = = ="
+  end
 
-BASE_URL = "https://www.amazon.co.jp/s?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&i=aps&k=#{keyword}&ref=nb_sb_noss&url=search-alias%3Daps"
+  def not_availability_last_page
+    "= = = = = = = = = = LAST PAGE NOT AVAILABILITY = = = = = = = = = ="
+  end
 
-raw_page = Nokogiri::HTML(open(BASE_URL))
-items = raw_page.css("#a-page > #search > .sg-row > .sg-col > .sg-col-inner > .rush-component > .s-result-list .s-result-item")
-asin_array = []
-items.each do |item|
-  asin = item.attr("data-asin")
-  unless asin == nil
-    asin_array << asin
+  def complete_get_asin
+    "= = = = = = = = = = COMPLETE GET DATA ASIN = = = = = = = = = = = ="
+  end
+
+  def insert_to_csv
+    "= = = = = = = = = = INSERT TO CSV FILE = = = = = = = = = = = = = ="
+  end
+
+  def complete
+    "= = = = = = = = = = COMPLETE = = = = = = = = = = = = = = = = = = ="
+  end
+
+  def loading
+    "== "
   end
 end
-  puts asin_array
 
-CSV.open('production.csv', 'wb') do |csv|
-  csv << asin_array
+class InsertData < NoticeDefault
+    attr_accessor :keyword, :last_page
+
+    def initialize
+      puts insert_keyword
+      @keyword = gets.chomp.to_s
+      puts insert_last_page
+      @last_page = gets.chomp.to_i
+    end
 end
+
+
+class GetdataAsin
+  attr_accessor :urls
+  
+  def initialize(insert_data)
+    @urls = []
+    $total_asin = []
+    insert_data.last_page.times {|i| urls.push("https://www.amazon.co.jp/s?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&i=aps&k=#{insert_data.keyword}&page=#{i+1}&ref=nb_sb_noss&url=search-alias%3Daps")}
+  end
+end
+
+class Parser < NoticeDefault
+
+  def initialize(insert_data)
+    ## Function
+    insert_data.urls.each { |url|
+      raw_page = Nokogiri::HTML(open(url))
+      items = raw_page.css("#a-page > #search > .sg-row > .sg-col > .sg-col-inner > .rush-component > .s-result-list .s-result-item")
+      asin_array = []
+      items.each do |item|
+        asin = item.attr("data-asin")
+        unless asin == nil
+          asin_array << asin
+        end
+      end
+      $total_asin = $total_asin + asin_array
+      print loading
+    }
+    p complete_get_asin
+    p insert_to_csv
+  end
+end
+
+class InsertCsv < NoticeDefault
+  def initialize(insert_data)
+    CSV.open("#{insert_data.keyword}_asin_amazonjp.csv", 'wb') do |csv|
+      $total_asin.each do |item|
+        csv << [item, "\n"]
+      end
+    end
+    puts complete
+  end
+end
+
+insert_data = InsertData.new
+get_data = GetdataAsin.new(insert_data)
+parser_data = Parser.new(get_data)
+InsertCsv.new(insert_data)
